7e9e334290e890dd792fbf87a1e46aad5b591d4b
koalix <oskeruse@gmail.com>
2015-04-04 22:21:46 +0200

    Object game added.

---
+++ src/game/game.js
@@ -0,0 +1,91 @@
+/*global marriage */
+(function () {
+	"use strict";
+
+	marriage.game = marriage.game || {};
+
+	/**
+	 * Game type.
+	 * @enum {string}
+	 */
+	marriage.game.Type = {
+		elective: "volený",
+		bidden: "licitovaný",
+		draw: "lízaný",
+		cross: "køížový"
+	};
+
+
+	/**
+	 * Assign roles to players.
+	 * @param {Array.<marriage.Player>} players
+	 * @param {Array.<marriage.Role>} roles
+	 */
+	function assignRoles(players, roles) {
+		players.forEach(function(player, index) {
+			player.setRole(roles[index]);
+		});
+	}
+
+
+	/**
+	 * Creates a new instance
+	 * @constructor
+	 */
+	marriage.Game = function (type) {
+		/**
+		 * @private
+		 * @type {marriage.game.Type}
+		 */
+		this.type = type;
+		/**
+		 * @private
+		 * @type {marriage.Dispatcher}
+		 */
+		this.dispatcher = new marriage.Dispatcher();
+
+		/**
+		 * @private
+		 * @type {Array.<marriage.Player>}
+		 */
+		this.players = [
+			new marriage.Player("A"),
+			new marriage.Player("B"),
+			new marriage.Player("C")
+		];
+
+		assignRoles(this.players, [
+			new marriage.role.Foreman(),
+			new marriage.role.Centre(),
+			new marriage.role.Defender()
+		]);
+
+		// 1. defender shuffles thr pack
+		// 2. centre cuts the pack
+		// 3. defender deals the cards - 7 to foreman, 5 to centre, 5 to oneself and then again 5, 5, 5
+		// 4. foreman takes first group of seven cards and selects trump (or do the election from pack of 5 cards)
+		// 5. foreman creates the talon and asks others for the contract
+		// 6. when answer from both (centre and defender) is ok, foreman shows trump and announce contract
+		// 7. contract can be suit, betl or durch
+		// 8. there can be some bonuses - 7, 107 (7 against)
+		// 9. Flek
+		// 10. game
+
+		// Betl / durch
+		// When somebody picks up the talon, it means that contract is rejected and he wants to play betl or durch
+		// If betl has been announced, another player can reject it and play durch
+	};
+
+	/**
+	 * Switch roles.
+	 */
+	marriage.Game.prototype.switchRoles = function () {
+		var roles = [];
+
+		this.players.forEach(function (player) {
+			roles.push(player.getRole());
+		});
+		roles.unshift(roles.pop());
+		assignRoles(this.players, roles);
+	};
+}());
---
+++ test/game/game.js
@@ -0,0 +1,64 @@
+/*global marriage, describe, it, expect */
+describe("The game", function() {
+	"use strict";
+
+	/**
+	 * Returns role name of player on specified index
+	 * @param {Array.<marriage.Player>} players
+	 * @param {number} index
+	 * @return {marriage.role.RoleName}
+	 */
+	function getRoleNameOnIndex(players, index) {
+		return players[index].getRole().getRoleName();
+	}
+
+	describe("The types", function () {
+		it("Expected game types exist", function () {
+			expect(marriage.game.Type.bidden).toBe("licitovaný");
+			expect(marriage.game.Type.cross).toBe("køížový");
+			expect(marriage.game.Type.draw).toBe("lízaný");
+			expect(marriage.game.Type.elective).toBe("volený");
+		});
+	});
+
+	it("Initial order of roles in game 'volený' are 'forhont, støední hráè, zadák'", function () {
+		//noinspection JSAccessibilityCheck
+		var game = new marriage.Game(marriage.game.Type.elective),
+			players = game.players;
+
+		expect(getRoleNameOnIndex(players, 0)).toBe(marriage.role.RoleName.foreman);
+		expect(getRoleNameOnIndex(players, 1)).toBe(marriage.role.RoleName.centre);
+		expect(getRoleNameOnIndex(players, 2)).toBe(marriage.role.RoleName.defender);
+	});
+
+	it("Roles rotate regularly", function () {
+		//noinspection JSAccessibilityCheck
+		var game = new marriage.Game(marriage.game.Type.elective),
+			players = game.players;
+
+		expect(getRoleNameOnIndex(players, 0)).toBe(marriage.role.RoleName.foreman);
+		expect(getRoleNameOnIndex(players, 1)).toBe(marriage.role.RoleName.centre);
+		expect(getRoleNameOnIndex(players, 2)).toBe(marriage.role.RoleName.defender);
+
+		game.switchRoles();
+		//noinspection JSAccessibilityCheck
+		players = game.players;
+		expect(getRoleNameOnIndex(players, 0)).toBe(marriage.role.RoleName.defender);
+		expect(getRoleNameOnIndex(players, 1)).toBe(marriage.role.RoleName.foreman);
+		expect(getRoleNameOnIndex(players, 2)).toBe(marriage.role.RoleName.centre);
+
+		game.switchRoles();
+		//noinspection JSAccessibilityCheck
+		players = game.players;
+		expect(getRoleNameOnIndex(players, 0)).toBe(marriage.role.RoleName.centre);
+		expect(getRoleNameOnIndex(players, 1)).toBe(marriage.role.RoleName.defender);
+		expect(getRoleNameOnIndex(players, 2)).toBe(marriage.role.RoleName.foreman);
+
+		game.switchRoles();
+		//noinspection JSAccessibilityCheck
+		players = game.players;
+		expect(getRoleNameOnIndex(players, 0)).toBe(marriage.role.RoleName.foreman);
+		expect(getRoleNameOnIndex(players, 1)).toBe(marriage.role.RoleName.centre);
+		expect(getRoleNameOnIndex(players, 2)).toBe(marriage.role.RoleName.defender);
+	});
+});
