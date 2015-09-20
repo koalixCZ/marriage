eef006a0ed21daed8f54a355a00c4f69e4a63884
koalix <oskeruse@gmail.com>
2015-09-20 10:15:51 +0200

    New events have been added.

---
+++ src/event/event.start.js
@@ -0,0 +1,16 @@
+/*global marriage*/
+
+(function () {
+	"use strict";
+
+	marriage.event = marriage.event || {};
+
+	/**
+	 * Creates a new instance of the 'start' event.
+	 * @constructor
+	 */
+	marriage.event.Start = function () {
+	};
+
+	marriage.event.Start.prototype = new marriage.Event(marriage.EventName.start);
+}());
---
+++ src/event/event.up.js
@@ -0,0 +1,16 @@
+/*global marriage*/
+
+(function () {
+	"use strict";
+
+	marriage.event = marriage.event || {};
+
+	/**
+	 * Creates a new instance of the 'pre' event.
+	 * @constructor
+	 */
+	marriage.event.Up = function () {
+	};
+
+	marriage.event.Up.prototype = new marriage.Event(marriage.EventName.up);
+}());
